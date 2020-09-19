53a3ca682c0df388a69ff590e286bf92e3c9741c
Alexandre Castro <im.alexandre07@gmail.com>
2020-09-19 19:26:01 -0300

    configurações dwm

---
+++ scripts/bateria.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+while [[ true ]]; do
+    bateria=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{ print $2}'`
+    som=`awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master)`
+    hora=`date +"%A-%d de %B | %T"`
+    xsetroot -name "Bateria: $bateria | Volume: $som | $hora"
+    sleep 0.5s
+done
+
+
+
