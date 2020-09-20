c5096ff19162e57c25ecb48b679f4e2b06b04e32
Alexandre Castro <im.alexandre07@gmail.com>
2020-09-20 18:19:33 -0300

    neofetch e autostart update

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -5,9 +5,9 @@ nitrogen --restore &
 while [[ true ]]; do
     bateria=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{ print $2}'`
     som=`awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master)`
-    hora=`date +"%A-%d%b | %H:%M"`
+    hora=`date +"%A-%d%b | %T"`
     xsetroot -name "  🔋 $bateria | 🔊 $som | $hora  "
-    sleep 3s
+    sleep 5s
 done
 
 
