c7dde22bd993a0537219c0c27c6fa331e4461171
Alexandre Castro <im.alexandre07@gmail.com>
2020-09-20 17:43:22 -0300

    inclusão do dwm

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-#xcompmgr &
+xcompmgr &
 nitrogen --restore &
 while [[ true ]]; do
     bateria=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{ print $2}'`
     som=`awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master)`
     hora=`date +"%A-%d%b | %H:%M"`
     xsetroot -name "  🔋 $bateria | 🔊 $som | $hora  "
-    sleep 0.5s
+    sleep 3s
 done
 
 
