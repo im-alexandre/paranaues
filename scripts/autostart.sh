198415c9cd05d9ce5875570e89b4cd34026ce27b
Alexandre Castro <im.alexandre07@gmail.com>
2020-09-20 23:21:34 -0300

    bateria

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -6,7 +6,11 @@ while [[ true ]]; do
     bateria=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{ print $2}'`
     som=`awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master)`
     hora=`date +"%A-%d%b | %T"`
-    xsetroot -name "  🔋 $bateria | 🔊 $som | $hora  "
+    if [[ ! -z $bateria ]]; then
+        xsetroot -name "  🔋 $bateria | 🔊 $som | $hora  "
+    else
+        xsetroot -name "  🔊 $som | $hora  "
+    fi
     sleep 5s
 done
 
