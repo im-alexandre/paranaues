3add22195e4cd0ef837a643506a2121b3e13d41e
Alexandre Castro <im.alexandre07@gmail.com>
2022-03-26 13:19:20 -0300

    -

--- vim/lua/user/cmp.lua
+++ vim/lua/user/cmp.lua
@@ -115,9 +115,9 @@ cmp.setup {
 
   -- Completion order
   sources = {
+      { name = "luasnip" },
       { name = "nvim_lsp" },
       { name = "nvim_lua" },
-      { name = "luasnip" },
       { name = "buffer" },
       { name = "path" },
   },
